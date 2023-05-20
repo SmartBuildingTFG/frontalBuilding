27c38cdf873ff2610844b55c7755053255abbae3
Alejandro Ruzafa <ruzafa-@outlook.es>
2023-05-20 18:42:05 +0200

    feat(home): make container glask

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className="flex flex-col min-h-screen">
     <Header />
     <div id="home-background" className="gap-5 w-full h-full flex flex-col grow">
-      <main className="container bg-gray-200 grow rounded-lg p-5 bg-opacity-70 max-w-7xl mx-auto mt-5">
+      <main className="container grow rounded-lg p-5 max-w-7xl mx-auto mt-5 glask">
         { children }
       </main>
       <Footer />
