8e913c0cd7589d2f0a4349577171712cd4d8e012
Alejandro Ruzafa <ruzafa-@outlook.es>
2023-07-10 20:01:21 +0200

    fix: update IDM endpoint

--- src/api/identity-manager.tsx
+++ src/api/identity-manager.tsx
@@ -1,6 +1,6 @@
 import { type LoginResponse } from './identity-manager.interface'
 
-const API_URL = 'http://orion-ld.ruzafa.me:3000/v1'
+const API_URL = 'http://orion-ld.ruzafa.me/v1'
 
 export const login = async (email: string, password: string): Promise<LoginResponse | null> => {
   return await fetch(`${API_URL}/auth/tokens`, {
